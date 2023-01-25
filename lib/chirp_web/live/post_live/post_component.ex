7e98382d19872af57f7fd6ccea1d23be1763a9e7
Sebastian Röder <sebroeder@users.noreply.github.com>
2023-01-25 22:14:35 +0100

    Style post component with TailwindCSS

--- lib/chirp_web/live/post_live/post_component.ex
+++ lib/chirp_web/live/post_live/post_component.ex
@@ -4,32 +4,35 @@ defmodule ChirpWeb.PostLive.PostComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div id={"post-#{@post.id}"} class="post">
-      <div class="row">
-        <div class="column column-10">
-          <div class="post-avatar"></div>
+    <div id={"post-#{@post.id}"} class="flex gap-4 rounded border p-4 mt-2 text-sm">
+      <div class="flex-none">
+        <img src="https://placekitten.com/160/160" alt="avatar" class="h-20 w-20 rounded" />
+      </div>
+      <div class="flex flex-col justify-between grow">
+        <div>
+          <p class="font-semibold">@<%= @post.username %></p>
+          <p class="pb-2"><%= @post.body %></p>
         </div>
-        <div class="column column-90 post-body">
-          <b>@<%= @post.username %></b>
-          <br/>
-          <%= @post.body %>
+        <div class="flex justify-between text-xs font-semibold">
+          <span>
+            <Heroicons.heart class="inline-block h-4 w-4 stroke-2" />
+            <%= @post.likes_count %>
+          </span>
+          <span>
+            <Heroicons.arrow_path_rounded_square class="inline-block h-4 w-4 stroke-2" />
+            <%= @post.reposts_count %>
+          </span>
+          <span>
+            <.link patch={~p"/posts/#{@post}/edit"}>
+              <Heroicons.pencil_square class="inline-block h-4 w-4 stroke-2 hover:text-brand" />
+            </.link>
+            <.link phx-click={JS.push("delete", value: %{id: @post.id})} data-confirm="Are you sure?">
+              <Heroicons.trash class="ml-2 inline-block h-4 w-4 stroke-2 hover:text-brand" />
+            </.link>
+          </span>
         </div>
       </div>
-      <div class="row">
-        <div class="column">
-          <i class="far fa-heart"></i>
-          <%= @post.likes_count %>
-        </div>
-        <div class="column">
-          <i class="far fa-retweet"></i>
-          <%= @post.reposts_count %>
-        </div>
-        <div class= "column">
-          <.link patch={~p"/posts/#{@post}/edit"}>Edit</.link>
-          <.link phx-click={JS.push("delete", value: %{id: @post.id})} data-confirm="Are you sure?">Delete</.link>
-        </div>
-      </div>
-     </div>
+    </div>
     """
   end
 end
